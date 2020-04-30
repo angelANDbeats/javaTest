61ade0b0556f70a479954b0bf6e73af0f19dc7df
angel <741889771@qq.com>
2020-04-30 17:36:02 +0800

    修改javaTest

--- src/com/first/git/HelloWord.java
+++ src/com/first/git/HelloWord.java
@@ -3,10 +3,12 @@ package com.first.git;
 public class HelloWord {
     public static void main(String[] args){
         System.out.println("Hello Word");
+        //hello Test
     }
 
 
     public void test(){
         int i = 0;
+        System.out.println(i);
     }
 }
