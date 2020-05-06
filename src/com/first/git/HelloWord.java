b811146d1d194a65c90c12e9bd6a00033ca29d1b
angel <741889771@qq.com>
2020-05-06 14:53:46 +0800

    1

--- src/com/first/git/HelloWord.java
+++ src/com/first/git/HelloWord.java
@@ -3,8 +3,7 @@ package com.first.git;
 public class HelloWord {
     public static void main(String[] args){
         System.out.println("Hello Word");
-        //hello Test
-        //测试
+        //1
     }
 
 
