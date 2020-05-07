b0e1360ccdda03b5cd532e7606d41e60d7da46af
angel <741889771@qq.com>
2020-05-07 11:01:49 +0800

    跨分支推送

--- src/com/first/git/HelloWord.java
+++ src/com/first/git/HelloWord.java
@@ -3,11 +3,6 @@ package com.first.git;
 public class HelloWord {
     public static void main(String[] args){
         System.out.println("Hello Word");
-        //4
-        //5
-        //准备测试
-        //1
-        //试试>>>>>>> origin/master
     }
 
 
